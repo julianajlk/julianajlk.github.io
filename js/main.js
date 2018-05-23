60612536ef9c29655bc24a0c0f175e1412728d7a
JULIANAJLK <JULIANAJLK@GMAIL.COM>
2018-05-23 09:46:15 -0300

    minor updates

--- js/main.js
+++ js/main.js
@@ -43,24 +43,3 @@ $('nav a').click(function (event) {
 
 
 
-//back to top button
-$('body').prepend('<a href="#" class="back-to-top">Back to Top</a>');
-
-var amountScrolled = 300;
-
-$(window).scroll(function() {
-  if ($(window).scrollTop() > amountScrolled) {
-    $('a.back-to-top').fadeIn('slow');
-  } else {
-    $('a.back-to-top').fadeOut('slow');
-  }
-});
-
-$('a.back-to-top, a.simple-back-to-top').click(function() {
-  $('body,html').animate({
-    scrollTop: 0
-  }, 'fast');
-  return false;
-});
-
-
