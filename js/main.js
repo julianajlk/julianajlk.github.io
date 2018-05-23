76aac32780e8ebff95fcc553ae411cfd5967420d
JULIANAJLK <JULIANAJLK@GMAIL.COM>
2018-05-23 00:21:51 -0300

    complete update new site

--- js/main.js
+++ js/main.js
@@ -10,107 +10,25 @@ $(window).resize(function(){
   }
 });
 
+// button click here
+var button = document.getElementById("clickhere"),
+  count = 0;
+button.onclick = function() {
+  count = count + 1;
+  button.innerHTML = "&#9825;&nbsp;" + count;
+};
 
-
-
-// fancybox - work
-  $(document).ready(function() {
-    $(".fancybox").fancybox({
-     afterClose:function(){
-      $('.hidden').hide();
-     }
-      });
-    $(".fancybox1").fancybox({
-     afterClose:function(){
-      $('.hidden').hide();
-     }
-      });
-    $(".fancybox2").fancybox({
-     afterClose:function(){
-      $('.hidden').hide();
-     }
-      });
-    $(".fancybox3").fancybox({
-     afterClose:function(){
-      $('.hidden').hide();
-     }
-      });
-    $(".fancybox4").fancybox({
-     afterClose:function(){
-      $('.hidden').hide();
-     }
-      });
-    $(".fancybox5").fancybox({
-     afterClose:function(){
-      $('.hidden').hide();
-     }
-      });
-  });
-
+var results = document.getElementById('count').innerHTML = count;
+function iLoveCode(results) {
+  if (results < 2) {
+    return "You like code!";
+  } else {
+    return "You love code!";
+  }
+}
   
-$('#image-1').click(function() {
-    for (var i = 1; i <7; i++) {
-      $(this).append('<a class="fancybox hidden" rel="group" href="images/jk-design-case-1-gallery-' + i + '.jpg"></a>')
-      }
-  });
 
-  $('#image-2').click(function() { //not working
-    for (var i = 1; i <7; i++) {
-      $(this).append('<a class="fancybox1 hidden" rel="group" href="images/jk-design-case-2-gallery-' + i + '.jpg"></a>')
-      }
-  });
-
-  $('#image-3').click(function() { //not working
-    for (var i = 1; i <7; i++) {
-      $(this).append('<a class="fancybox2 hidden" rel="group" href="images/jk-design-case-3-gallery-' + i + '.jpg"></a>')
-      }
-  });
-
-  $('#image-4').click(function() { //not working
-    for (var i = 1; i <7; i++) {
-      $(this).append('<a class="fancybox3 hidden" rel="group" href="images/jk-design-case-4-gallery-' + i + '.jpg"></a>')
-      }
-  });
-
-  $('#image-5').click(function() { //not working
-    for (var i = 1; i <6; i++) {
-      $(this).append('<a class="fancybox4 hidden" rel="group" href="images/jk-design-case-5-gallery-' + i + '.jpg"></a>')
-      }
-  });
-
-  $('#image-6').click(function() { //not working
-    for (var i = 1; i <7; i++) {
-      $(this).append('<a class="fancybox5 hidden" rel="group" href="images/jk-design-case-6-gallery-' + i + '.jpg"></a>')
-      }
-  });
-
-
-  // $(document).ready(function() {
-  //   $(".fancybox").fancybox({
-  //    afterClose:function(){
-  //     $('.hidden').hide();
-  //    }
-  //     });
-  //   $("fancybox1").fancybox({
-  //    afterClose:function(){
-  //     $('.hidden').hide();
-  //    }
-  //     });
-  // });
-
-
-  // $('#image-1').click(function() {
-  //   for (var i = 1; i <6; i++) {
-  //     $(this).append('<a class="fancybox hidden" rel="group" href="images/jk-design-case-1-vertical-' + i + '.jpg"></a>')
-  //     }
-  // });
-  
-  // $('#image-2').click(function() { //link correct images
-  //   for (var i = 1; i <6; i++) {
-  //     $(this).append('<a class="fancybox hidden" rel="group" href="images/jk-design-case-1-vertical-' + i + '.jpg"></a>')
-  //     }
-  // });
-
+ 
 
 // scrolling website
 var speed = 1000;
