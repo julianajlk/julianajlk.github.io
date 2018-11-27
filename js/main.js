95f72a80fba1d00ef28c5d3d4efce15392be9205
julianajlk <julianajlk@gmail.com>
2018-11-27 14:34:54 -0500

    First update as portfolio

--- js/main.js
+++ js/main.js
@@ -1,23 +1,21 @@
-
 // button click here
-var button = document.getElementById("clickhere"),
-  count = 0;
-button.onclick = function() {
-  count = count + 1;
-  button.innerHTML = "&#9825;&nbsp;" + count;
-};
-
+// var button = document.getElementById("clickhere"),
+//   count = 0;
+// button.onclick = function() {
+//   count = count + 1;
+//   button.innerHTML = "&#9825;&nbsp;" + count;
+// };
 
 // scrolling website
 var speed = 1000;
 
-$('nav a').click(function (event) {  
-  var target = $(this).attr('href');
-  $('html, body').animate({
-    scrollTop: $(target).offset().top
-   }, speed);
+$("nav a").click(function(event) {
+  var target = $(this).attr("href");
+  $("html, body").animate(
+    {
+      scrollTop: $(target).offset().top
+    },
+    speed
+  );
   event.preventDefault();
 });
-
-
-
